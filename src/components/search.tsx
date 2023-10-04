567c8ccfdfeb776a0b010fb93417abe95afb7639
Summer <swatson9008@yahoo.com>
2023-10-04 12:06:22 -0400

    creating sorting algorithims for searching

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -4,7 +4,7 @@ import { PokemonClient } from "pokenode-ts";
 
 interface PokemonData {
   pokeName: string;
-  pokeMove: string[];
+  pokeMoves: Record<string, string[]>; 
 }
 
 export default function Search() {
@@ -21,16 +21,26 @@ export default function Search() {
     api
       .getPokemonByName(pokeSearch)
       .then((data) => {
+        const movesByVersionGroup: Record<string, string[]> = {};
 
-        const moveNames = data.moves.map((move) => move.name);
+        data.moves.forEach((move) => {
+          move.version_group_details.forEach((groupDetail) => {
+            const versionGroupName = groupDetail.version_group.name;
+            if (!movesByVersionGroup[versionGroupName]) {
+              movesByVersionGroup[versionGroupName] = [];
+            }
+            movesByVersionGroup[versionGroupName].push(move.move.name);
+          });
+        });
 
         const searchedPokemon: PokemonData = {
           pokeName: data.name,
-          pokeMove: moveNames,
+          pokeMoves: movesByVersionGroup,
         };
 
         setPokeData([searchedPokemon]);
         console.log(data);
+        console.log(movesByVersionGroup);
       })
       .catch((error) => {
         console.error(error);
@@ -56,9 +66,16 @@ export default function Search() {
           <div key={pokemon.pokeName}>
             <div className="pokeName">{pokemon.pokeName}</div>
             <div className="pokeMoves">
-              {pokemon.pokeMove.map((move) => (
-                <div key={move} className="pokeMove">
-                  {move}
+              {Object.keys(pokemon.pokeMoves).map((versionGroup) => (
+                <div key={versionGroup}>
+                  <div className="versionGroupName">{versionGroup}</div>
+                  <div className="movesList">
+                    {pokemon.pokeMoves[versionGroup].map((move) => (
+                      <div key={move} className="pokeMove">
+                        {move}
+                      </div>
+                    ))}
+                  </div>
                 </div>
               ))}
             </div>
