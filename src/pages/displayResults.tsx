5c96d74a0bfe17754b1bf73b9d6910b236bd9e6a
Summer <swatson9008@yahoo.com>
2023-10-06 16:33:15 -0400

    put conditional formatting for displayResults

--- src/pages/displayResults.tsx
+++ src/pages/displayResults.tsx
@@ -1,49 +1,35 @@
-import formatString from '../components/formatString';
-import { PokemonData } from '../components/pokemonData';
-import sortMoves from '../components/sortMove';
+import { useState } from "react";
+import formatString from "../components/formatString";
+import { PokemonData } from "../components/pokemonData";
+import sortMoves from "../components/sortMove";
 
 interface DisplayResultsProps {
-    pokeData: PokemonData;
-  }
+  pokeData: PokemonData;
+}
 
 export default function DisplayResults({ pokeData }: DisplayResultsProps) {
-  const sortedData = sortMoves(pokeData)
+  const sortedData = sortMoves(pokeData);
+  const defaultGameTitle = Object.keys(sortedData.pokeMoves)[1];
+  const [gameTitle, setGameT] = useState(defaultGameTitle);
+  const [learnMethod, setMethod] = useState("level-up");
+  const moveList = sortedData.pokeMoves[gameTitle][learnMethod] || [];
+  console.log(moveList);
   return (
     <div className="searchMain">
       <div className="displayResult">
         <div key={sortedData.pokeName}>
-          <div className="pokeName">{sortedData.pokeName}</div>
+          <div className="pokeName">{formatString(sortedData.pokeName)}</div>
           <div className="pokeMoves">
-            {Object.keys(sortedData.pokeMoves).map((versionGroup) => (
-              <div key={versionGroup}>
-                <div className="versionGroupName">{formatString(versionGroup)}</div>
-                <div className="moveLearnMethods">
-                  {Object.keys(sortedData.pokeMoves[versionGroup]).map(
-                    (moveLearnMethod) => {
-                      if (moveLearnMethod === "level-up") {
-                        return (
-                          <div key={moveLearnMethod}>
-                            <div className="moveLearnMethod">
-                              {formatString(moveLearnMethod)}
-                            </div>
-                            <div className="movesList">
-                              {sortedData.pokeMoves[versionGroup][moveLearnMethod].map((move) => (
-                                <div key={move.name} className="pokeMove">
-                                  {move.level && `Level: ${move.level} `}
-                                  {formatString(move.name)}
-                                </div>
-                              ))}
-                            </div>
-                          </div>
-                        );
-                      } else {
-                        return null;
-                      }
-                    }
-                  )}
+            <div className="gameTitle">{formatString(gameTitle)}</div>
+            <div className="learnMethod">{formatString(learnMethod)}</div>
+            <div className="moveList">
+              {moveList.map((move) => (
+                <div key={move.name} className="pokeMove">
+                  {move.level && `Level: ${move.level} `}
+                  {formatString(move.name)}
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
         </div>
       </div>
