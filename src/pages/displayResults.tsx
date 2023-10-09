6a33eab053af1fab6761cbcce268239f26197af3
Summer <swatson9008@yahoo.com>
2023-10-09 14:15:07 -0400

    working on algorithim to pull TM/HM #s

--- src/pages/displayResults.tsx
+++ src/pages/displayResults.tsx
@@ -1,7 +1,9 @@
-import { useState } from "react";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useState, useEffect } from "react";
 import formatString from "../components/formatString";
 import { PokemonData } from "../components/pokemonData";
 import sortMoves from "../components/sortMove";
+import { MoveClient } from 'pokenode-ts';
 
 interface DisplayResultsProps {
   pokeData: PokemonData;
@@ -16,7 +18,7 @@ export default function DisplayResults({ pokeData }: DisplayResultsProps) {
   const learnList = Object.keys(sortedData.pokeMoves[gameTitle] || {});
 
 
-  const customLearnMethodOrder = ["level-up", "machine", "egg", "tutor"];
+  const customLearnMethodOrder = ["level-up", "machine", "egg", "tutor", "stadium-surfing-pikachu"];
 
   learnList.sort((a, b) => {
     const indexA = customLearnMethodOrder.indexOf(a);
@@ -31,6 +33,65 @@ export default function DisplayResults({ pokeData }: DisplayResultsProps) {
   const handleLearnMethod = (method: string) => {
     setLearnMethod(method);
   };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        if (learnMethod === "machine") {
+          const moveClient = new MoveClient();
+          const data = await moveClient.getMoveByName('return');
+  
+
+          const matchingMachine = data.machines.find(
+            (machine) => machine.version_group.name === gameTitle
+          );
+  
+
+          if (matchingMachine) {
+            const url = matchingMachine.machine.url;
+            const response = await fetch(url);
+            const moveRes = await response.json();
+            const moveTM = moveRes.item.name;
+            console.log(moveTM);
+          } else {
+            console.log(`No machine found with version group name ${gameTitle}`);
+          }
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+  
+    fetchData();
+  }, [gameTitle, learnMethod]);
+  
+  
+
+  /*useEffect(() => {
+    const fetchData = async () => {
+      try {
+        if (learnMethod === "machine") {
+          const moveClient = new MoveClient();
+          const data = await moveClient.getMoveByName('return');
+          //const filteredGame = data.filter(moves => moves.machine.version_group === gameTitle)
+          //console.log(filteredGame)
+          const url = data.machines[0].machine.url;
+          const response = await fetch(url); 
+          const moveRes = await response.json();
+          const moveTM = moveRes.item.name;
+          console.log(data.machines)
+          console.log(data);
+          console.log(moveTM);
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+  
+    fetchData();
+  }, [gameTitle, learnMethod]);*/
+   
+
 
   return (
     <div className="searchMain">
