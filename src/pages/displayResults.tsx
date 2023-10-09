f17b24f7952b02312cc47e11dc9baea4a3341712
Summer <swatson9008@yahoo.com>
2023-10-09 11:02:32 -0400

    created title and method algos

--- src/pages/displayResults.tsx
+++ src/pages/displayResults.tsx
@@ -10,17 +10,48 @@ interface DisplayResultsProps {
 export default function DisplayResults({ pokeData }: DisplayResultsProps) {
   const sortedData = sortMoves(pokeData);
   const defaultGameTitle = Object.keys(sortedData.pokeMoves)[0];
-  const [gameTitle, setGameT] = useState(defaultGameTitle);
-  const [learnMethod, setMethod] = useState("level-up");
+  const [gameTitle, setGameTitle] = useState(defaultGameTitle);
+  const [learnMethod, setLearnMethod] = useState("level-up");
   const moveList = sortedData.pokeMoves[gameTitle][learnMethod] || [];
+  const learnList = Object.keys(sortedData.pokeMoves[gameTitle] || {});
+
+  const handleTitle = (title: string) => {
+    setGameTitle(title);
+  };
+
+  const handleLearnMethod = (method: string) => {
+    setLearnMethod(method);
+  };
+
   return (
     <div className="searchMain">
       <div className="displayResult">
         <div key={sortedData.pokeName}>
           <div className="pokeName">{formatString(sortedData.pokeName)}</div>
           <div className="pokeMoves">
+            <div className="pokeTitleList">
+              {Object.keys(sortedData.pokeMoves).map((title, index) => (
+                <div
+                  key={index}
+                  className={index.toString()}
+                  onClick={() => handleTitle(title)}
+                >
+                  {formatString(title)}
+                </div>
+              ))}
+            </div>
             <div className="gameTitle">{formatString(gameTitle)}</div>
-            <div className="learnMethod">{formatString(learnMethod)}</div>
+            <div className="learnMethodList">
+              {learnList.map((method, index) => (
+                <div
+                  key={index}
+                  className={method === learnMethod ? "selected" : ""}
+                  onClick={() => handleLearnMethod(method)}
+                >
+                  {formatString(method)}
+                </div>
+              ))}
+            </div>
             <div className="moveList">
               {moveList.map((move) => (
                 <div key={move.name} className="pokeMove">
