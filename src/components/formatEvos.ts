8ca78aadfa730e3e0c7e60a4a983a4c7fce7df49
Summer <swatson9008@yahoo.com>
2023-10-15 19:45:20 -0400

    code optimizations

--- src/components/formatEvos.ts
+++ src/components/formatEvos.ts
@@ -5,35 +5,42 @@ export default function formatEvos(evolutionChain: EvolutionChain) {
   const triggerMethod =
     evolutionChain.chain.evolves_to[0].evolution_details[0].trigger.name;
 
+  const evoShortcut = evolutionChain.chain.evolves_to[0].evolution_details[0];
+
+
   const firstStageEvoMethods = {
-    minLevel: evolutionChain.chain.evolves_to[0].evolution_details[0]?.min_level,
-    minAfection: evolutionChain.chain.evolves_to[0].evolution_details[0]?.min_affection,
-    itemHeld: evolutionChain.chain.evolves_to[0].evolution_details[0]?.held_item,
-    minHappiness: evolutionChain.chain.evolves_to[0].evolution_details[0]?.min_happiness,
-    genderCheck: evolutionChain.chain.evolves_to[0].evolution_details[0]?.gender,
-    minBeauty: evolutionChain.chain.evolves_to[0].evolution_details[0]?.min_beauty,
-    timeOfDay: evolutionChain.chain.evolves_to[0].evolution_details[0]?.time_of_day,
-    overworldRain: evolutionChain.chain.evolves_to[0].evolution_details[0]?.needs_overworld_rain,
-    knownMove: evolutionChain.chain.evolves_to[0].evolution_details[0]?.known_move
+    minLevel: evoShortcut?.min_level,
+    minAfection: evoShortcut?.min_affection,
+    itemHeld: evoShortcut?.held_item,
+    minHappiness: evoShortcut?.min_happiness,
+    genderCheck: evoShortcut?.gender,
+    minBeauty: evoShortcut?.min_beauty,
+    timeOfDay: evoShortcut?.time_of_day,
+    overworldRain: evoShortcut?.needs_overworld_rain,
+    knownMove: evoShortcut?.known_move,
   };
 
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   function sliceEvoMethods(obj: any, keyValue: number) {
-    const keys = Object.keys(obj);
+    const keys = Object.keys(obj) as Array<keyof typeof firstStageEvoMethods>;
     const firstKey = keys[keyValue];
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { [firstKey]: _, ...rest } = obj;
     return rest;
   }
-  
 
   if (
     triggerMethod === "level-up" &&
-    Object.values(sliceEvoMethods(firstStageEvoMethods, 0)).every(prop => prop === null || prop === "" || prop === false)
+    Object.values(sliceEvoMethods(firstStageEvoMethods, 0)).every(
+      (prop) => !prop
+    )
   ) {
     return `${formatString(
       evolutionChain.chain.species.name
     )} evolves to ${formatString(
       evolutionChain.chain.evolves_to[0].species.name
-    )} at level ${firstStageEvoMethods.minLevel?.toString() || 'not known'}`;
+    )} at level ${firstStageEvoMethods.minLevel?.toString() || "not known"}`;
   }
 
   return "";
