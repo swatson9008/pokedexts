aab363a32c95eb75de129e78b19442b001001f85
Summer <swatson9008@yahoo.com>
2023-10-28 22:12:57 -0400

    added in some trainer sprites for a future update

--- README.md
+++ README.md
@@ -25,3 +25,24 @@ If you are developing a production application, we recommend updating the config
 - Replace `plugin:@typescript-eslint/recommended` to `plugin:@typescript-eslint/recommended-type-checked` or `plugin:@typescript-eslint/strict-type-checked`
 - Optionally add `plugin:@typescript-eslint/stylistic-type-checked`
 - Install [eslint-plugin-react](https://github.com/jsx-eslint/eslint-plugin-react) and add `plugin:react/recommended` & `plugin:react/jsx-runtime` to the `extends` list
+
+
+sprite credits
+
+serena - kyledove
+calem - Brumirage
+ello - ZacWeavile
+selene - Beliot419
+chase - Brumirage
+elaine - Brumirage
+victor - Brumirage
+gloria - Brumirage
+rei - Brumirage
+akari - Brumirage
+florian - kyledove
+juliana - kyledove
+
+
+
+
+
--- src/components/randomizedSprites.tsx
+++ src/components/randomizedSprites.tsx
@@ -1,14 +1,45 @@
-export default function RandomizedSprites(){
+import { useState, useEffect } from "react";
+import { Animate } from "react-simple-animate";
 
-    const pickARandomSprite = () => {
-        const pokeNo = Math.floor(Math.random() * (1010 - 1 + 1) + 1)
-        return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeNo}.png`
-    }
+export default function RandomizedSprites() {
+  const slideDuration = 5000;
 
-    return(
-        <>
-        <img src={pickARandomSprite()} alt=""/>
-        </>
+  const pickARandomSprite = () => {
+    const pokeNo = Math.floor(Math.random() * (1010 - 1 + 1) + 1);
+    return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeNo}.png`;
+  };
 
-    )
+  const [image1, setImage1] = useState(pickARandomSprite());
+  const [image2, setImage2] = useState(pickARandomSprite());
+  const [showImage1, setShowImage1] = useState(true);
+
+  const onAnimationStart = { opacity: 0, transform: "translateX(-100%)" };
+  const onAnimationEnd = { opacity: 1, transform: "translateX(0)" };
+
+  useEffect(() => {
+    const changeSpriteTimeout = setTimeout(() => {
+      if (showImage1) {
+        setImage2(pickARandomSprite());
+      } else {
+        setImage1(pickARandomSprite());
+      }
+      setShowImage1(!showImage1);
+    }, slideDuration);
+
+    return () => clearTimeout(changeSpriteTimeout);
+  }, [showImage1]);
+
+  return (
+    <div className="sprite-container">
+      <Animate
+        play={showImage1}
+        start={onAnimationStart}
+        end={onAnimationEnd}
+        duration={slideDuration}
+        render={({ style }) => (
+          <img style={style} src={showImage1 ? image1 : image2} alt="" />
+        )}
+      />
+    </div>
+  );
 }
