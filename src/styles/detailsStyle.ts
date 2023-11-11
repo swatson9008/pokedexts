ec0cc16534d6df016db121dc71f444b3f7c49fc0
Summer <swatson9008@yahoo.com>
2023-11-11 17:27:07 -0500

    made the searchbar sticky

--- src/styles/detailsStyle.ts
+++ src/styles/detailsStyle.ts
@@ -4,7 +4,6 @@ export const DetailsPage = styled.div`
 
 display: flex;
 flex-direction: column;
-margin-top: 20px;
 
 
 > div.searchMain > div {
@@ -19,11 +18,24 @@ margin-top: 20px;
 
 
   @media only screen and (min-device-width: 769px) {
+    margin-top: 20px;
     > div.searchMain > div {
     display: flex;
     justify-content: center;
     flex-direction: row;
     align-items: flex-end;
 }
+  }
+
+  @media only screen and (max-device-width: 768px) and (orientation: portrait) {
+    .searchMain {
+      position: sticky;
+      top: 0;
+      padding-top: 10px;
+      padding-bottom: 10px;
+      z-index: 100;
+      width: 100vw;
+      background-color: rgb(164 234 123);
+    }
   }
 `
