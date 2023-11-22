e78b4b1d3c362047a94a5eb8888f08e0aff46ed0
Summer <swatson9008@yahoo.com>
2023-11-22 12:17:34 -0500

    reverted change

--- src/styles/footerStyle.ts
+++ src/styles/footerStyle.ts
@@ -2,20 +2,18 @@ import styled from "styled-components";
 import IsDarkMode from "./isDarkModeInferface";
 
 export const FooterBox = styled.div<IsDarkMode>`
-box-sizing: border-box;
   transition: color 0.2s linear;
-  color: ${(props) => (props.isDarkMode ? "#c9c9c9" : "#5e5e5e")};
+  color: ${(props) =>
+    props.isDarkMode ? "#c9c9c9" : "#5e5e5e"};
   display: flex;
   flex-direction: column;
+  margin-top: 50px;
+  margin-bottom: 15px;
   justify-content: center;
   align-items: center;
   text-align: center;
-  font-size: 13px;
-  position: fixed;
-  left: 0;
-  bottom: 0;
-  width: 100%;
-  padding: 15px;
+  font-size: 15px;
+  
 
   @media only screen and (max-width: 799px) {
     > span {
