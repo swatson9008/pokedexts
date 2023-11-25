abd01c59e197847ad3f512a8dc79d52bf7446c5e
Summer <swatson9008@yahoo.com>
2023-11-25 13:01:40 -0500

    removed an out of place symbol

--- src/pages/displayResults.tsx
+++ src/pages/displayResults.tsx
@@ -689,7 +689,6 @@ export default function DisplayResults({ pokeData }: DisplayResultsProps) {
                     </>
                   )}
                 </div>
-                ;
               </LearnMethodStyle>
             </div>
           </EntireDetailPage>
